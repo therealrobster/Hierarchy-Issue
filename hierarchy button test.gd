fe23b70c0ca1b532a8265241daba908681616958
Unknown <robertstarr76@gmail.com>
2017-11-22 17:18:24 +1000

    initial commit

---
+++ hierarchy button test.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
+func _on_Button_pressed():
+	var scene = load("res://Hierarcy Test Scene 1.tscn")
+	var node = scene.instance()
+	add_child(node)
+
